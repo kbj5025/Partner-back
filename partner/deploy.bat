fe581b2bc9f519c6f97e03d823d7f34469bba5c4
KO BOUNG JUN <koboungjun94@gmail.com>
2021-11-25 11:30:50 +0900

    1

--- partner/deploy.bat
+++ partner/deploy.bat
@@ -2,3 +2,8 @@
 scp -i "c:partner.pem" -r ./minam/build/libs/*.jar ubuntu@ec2-3-36-125-183.ap-northeast-2.compute.amazonaws.com:/home/ubuntu/app/partner
 @rem ===== 2. 기존 프로세스 종료
 scp -i "c:partner.pem" ubuntu@15.164.54.22 "pkill -9 -f java"
+
+@rem ===== 3. dev프로필로 jar 파일 실행
+ssh -i "c:partner.pem" ubuntu@15.164.54.22 "cd /home/ubuntu/partner; nohup java -Dspring.profiles.active=dev -jar partner*.jar 1>partner.log 2>&1 &"
+
+
--- partner/minam/src/main/java/com/example/minam/configuration/WebConfiguration.java
+++ partner/minam/src/main/java/com/example/minam/configuration/WebConfiguration.java
@@ -11,7 +11,7 @@ public class WebConfiguration implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry
 			.addMapping("/**")
-			.allowedOrigins("http://localhost:3000",
+			.allowedOrigins("http://localhost:3000","http://127.0.0.1:5500/",
 	                  // Áö¿ø
 //	                  "http://ec2-15-164-211-184.ap-northeast-2.compute.amazonaws.com:8080", "http://3.35.51.8:3000",
 //	                  "http://3.35.51.8", "http://ec2-3-35-51-8.ap-northeast-2.compute.amazonaws.com:3000",
